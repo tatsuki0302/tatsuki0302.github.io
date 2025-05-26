29bc4a3446a08da99e1deb6a91409e04b52e5c26
tatsuki0302 <yamamoto.tatsuki.25d@st.kyoto-u.ac.jp>
2025-05-27 01:14:16 +0900

    Create RECENT WORKS and SKILL sections

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 // ページトップに戻るアニメーション
 document.addEventListener("DOMContentLoaded", function () {
-    const pageTop = document.getElementById("js-page-top");
-    if (pageTop) {
-      pageTop.addEventListener("click", function () {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      });
-    }
+  const pageTop = document.getElementById("js-page-top");
+  if (pageTop) {
+    pageTop.addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  }
+
+  // PRESENTATIONS に番号を振る
+  const presentationList = document.querySelectorAll('#presentation-list .work-item .work-name');
+  presentationList.forEach((item, index) => {
+    item.innerHTML = `[${index + 1}] ` + item.innerHTML;
   });
-  
+
+  // AWARDS に番号を振る
+  const awardList = document.querySelectorAll('#award-list .work-item .work-name');
+  awardList.forEach((item, index) => {
+    item.innerHTML = `[${index + 1}] ` + item.innerHTML;
+  });
+});
