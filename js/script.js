49a57e7e39cc8579f2b71d1df5ee3f54b8d70d73
Tatsuki Yamamoto <yamamoto.tatsuki.25d@st.kyoto-u.ac.jp>
2026-01-26 13:28:21 +0900

    Update: added statistics

--- js/script.js
+++ js/script.js
@@ -5,20 +5,4 @@ document.addEventListener("DOMContentLoaded", function () {
       window.scrollTo({ top: 0, behavior: "smooth" });
     });
   }
-
-  // PRESENTATIONS に番号を振る
-  const presentationItems = document.querySelectorAll('#presentation-list .work-name');
-  presentationItems.forEach((item, index) => {
-    if (!item.innerHTML.startsWith("[")) {
-      item.innerHTML = `[${index + 1}] ` + item.innerHTML;
-    }
-  });
-
-  // AWARDS に番号を振る
-  const awardItems = document.querySelectorAll('#award-list .work-name');
-  awardItems.forEach((item, index) => {
-    if (!item.innerHTML.startsWith("[")) {
-      item.innerHTML = `[${index + 1}] ` + item.innerHTML;
-    }
-  });
 });
